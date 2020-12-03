054e2931b4e69fb3923d8351557dfc460995ef28
Kinanty <kinantykusuma@gmail.com>
2020-12-03 10:59:10 +0700

    add adapter & main menu layout

--- app/src/main/java/com/example/footballdb/MainMenu.java
+++ app/src/main/java/com/example/footballdb/MainMenu.java
@@ -1,8 +1,11 @@
 package com.example.footballdb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
     }
 }
