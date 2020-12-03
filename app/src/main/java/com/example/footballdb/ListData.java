986a2e55e88ac4224aa0cdcb97736ea339a6a0c7
Kinanty <kinantykusuma@gmail.com>
2020-12-03 18:00:44 +0700

    update tapi masi ngebug

---
+++ app/src/main/java/com/example/footballdb/ListData.java
@@ -0,0 +1,147 @@
+package com.example.footballdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+
+public class ListData extends AppCompatActivity {
+    ProgressDialog progressdialog;
+    private RecyclerView recyclerView;
+    private DataAdapter adapter;
+    private ArrayList<Model> DataArrayList; //kit add kan ke adapter
+    private ImageView tambah_data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_data);
+        progressdialog = new ProgressDialog(ListData.this);
+        recyclerView = (RecyclerView) findViewById(R.id.rvdata);
+        //addData();
+        addDataOnline();
+    }
+
+    void addData() {
+        //offline, isi data offline dulu
+        DataArrayList = new ArrayList<>();
+        Model data1 = new Model();
+        data1.setIdTeam("133604");
+        data1.setStrTeam("Arsenal");
+        data1.setStrTeamLogo("https:\\/\\/www.thesportsdb.com\\/images\\/media\\/team\\/logo\\/q2mxlz1512644512.png");
+        data1.setIntFormedYear("1892");
+        DataArrayList.add(data1);
+
+
+        adapter = new DataAdapter(DataArrayList, new DataAdapter.Callback() {
+            @Override
+            public void onClick(int position) {
+
+            }
+
+            @Override
+            public void test() {
+
+            }
+        });
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListData.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        //get data online
+
+
+    }
+
+    void addDataOnline() {
+        progressdialog.setMessage("Loading...");
+        progressdialog.show();
+        AndroidNetworking.get("https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League")
+                .setTag("test")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // do anything with response
+                        Log.d("hasiljson", "onResponse: " + response.toString());
+                        //jika sudah berhasil debugm lanjutkan code dibawah ini
+                        DataArrayList = new ArrayList<>();
+                        Model modelku;
+                        try {
+                            Log.d("hasiljson", "onResponse: " + response.toString());
+                            JSONArray jsonArray = response.getJSONArray("teams");
+                            Log.d("hasiljson2", "onResponse: " + jsonArray.toString());
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                modelku = new Model();
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                modelku.setIdTeam(jsonObject.getString("idTeam"));
+                                modelku.setStrTeam(jsonObject.getString("strTeam"));
+                                modelku.setStrTeamLogo(jsonObject.getString("strTeamBadge"));
+                                modelku.setIntFormedYear(jsonObject.getString("intFormedYear"));
+
+                                DataArrayList.add(modelku);
+                            }
+                            //untuk handle click
+                            adapter = new DataAdapter(DataArrayList, new DataAdapter.Callback() {
+                                @Override
+                                public void onClick(int position) {
+                                    Model football = DataArrayList.get(position);
+                                    Intent intent = new Intent(getApplicationContext(), DetailMovie.class);
+                                    intent.putExtra("id", football.idTeam);
+                                    intent.putExtra("judul", football.strTeam);
+                                    intent.putExtra("path", football.strTeamLogo);
+                                    intent.putExtra("date", football.intFormedYear);
+                                    startActivity(intent);
+                                    Toast.makeText(ListData.this, "" + position, Toast.LENGTH_SHORT).show();
+
+                                }
+
+                                @Override
+                                public void test() {
+
+                                }
+                            });
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListData.this);
+                            recyclerView.setLayoutManager(layoutManager);
+                            recyclerView.setAdapter(adapter);
+                            progressdialog.dismiss();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            progressdialog.dismiss();
+
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        Log.d("errorku", "onError errorCode : " + error.getErrorCode());
+                        Log.d("errorku", "onError errorBody : " + error.getErrorBody());
+                        Log.d("errorku", "onError errorDetail : " + error.getErrorDetail());
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/footballdb/Model.java
@@ -0,0 +1,54 @@
+package com.example.footballdb;
+
+public class Model {
+    String strTeam;
+    String strTeamLogo;
+    String idTeam;
+    String strDescriptionEN;
+    int intFormedYear;
+    String strCountry;
+    String strStadium;
+
+    public String getStrTeam(){
+        return strTeam;
+    }
+    public void setStrTeam(String strTeam) {
+        this.strTeam = strTeam;
+    }
+    public String getStrTeamLogo(){
+        return strTeamLogo;
+    }
+    public void setStrTeamLogo(String strTeamLogo) {
+        this.strTeamLogo = strTeamLogo;
+    }
+    public String getIdTeam(){
+        return idTeam;
+    }
+    public void setIdTeam(String idTeam) {
+        this.idTeam = idTeam;
+    }
+    public String getStrDescriptionEN(){
+        return strDescriptionEN;
+    }
+    public void setStrDescriptionEN(String strDescriptionEN) {
+        this.strDescriptionEN = strDescriptionEN;
+    }
+    public int getIntFormedYear(){
+        return intFormedYear;
+    }
+    public void setIntFormedYear(String strTeam) {
+        this.intFormedYear = intFormedYear;
+    }
+    public String getStrCountry(){
+        return strCountry;
+    }
+    public void setStrCountry(String strCountry) {
+        this.strCountry = strCountry;
+    }
+    public String getStrStadium(){
+        return strStadium;
+    }
+    public void setStrStadium(String strStadium) {
+        this.strStadium = strStadium;
+    }
+}
