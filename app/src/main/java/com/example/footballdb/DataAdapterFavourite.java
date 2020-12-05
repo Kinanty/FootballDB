391adcc9b3b3524955fe1394b45f0b199fada286
Kinanty <kinantykusuma@gmail.com>
2020-12-05 15:46:57 +0700

    update

--- app/src/main/java/com/example/footballdb/DataAdapterFavourite.java
+++ app/src/main/java/com/example/footballdb/DataAdapterFavourite.java
@@ -1,5 +1,6 @@
 package com.example.footballdb;
 
+import android.content.DialogInterface;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,11 +21,16 @@ import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class DataAdapterFavourite extends RecyclerView.Adapter<DataAdapterFavourite.DatakuViewHolder> {
     private List<ModelFootballRealm> dataList;
     private Callback callback;
     View viewku;
     int posku;
+    Realm realm;
+    RealmHelper realmHelper;
 
     interface Callback {
         void onClick(int position);
@@ -33,6 +40,9 @@ public class DataAdapterFavourite extends RecyclerView.Adapter<DataAdapterFavour
         this.callback = callback;
         this.dataList = dataList;
         Log.d("makanan", "MahasiswaAdapter: "+dataList.size()+"");
+        RealmConfiguration configuration = new RealmConfiguration.Builder().build();
+        realm = Realm.getInstance(configuration);
+        realmHelper = new RealmHelper(realm);
     }
     @Override
     public DatakuViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -102,7 +112,26 @@ public class DataAdapterFavourite extends RecyclerView.Adapter<DataAdapterFavour
 
                 case 2:
                     //Do stuff
+                    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which){
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    //Yes button clicked
+                                    realmHelper.delete(dataList.get(posku).getId());
+                                    notifyDataSetChanged();
+                                    break;
 
+                                case DialogInterface.BUTTON_NEGATIVE:
+                                    //No button clicked
+                                    break;
+                            }
+                        }
+                    };
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(viewku.getContext());
+                    builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                            .setNegativeButton("No", dialogClickListener).show();
                     break;
             }
             return true;
